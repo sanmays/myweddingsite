f6904f560e14f1a67f9a85a114f1c9fe160e9b72
sanmays <sanmay3290@gmail.com>
2022-11-04 01:08:42 +0100

    Added page for invitees of both events - Update 1

--- src/App.js
+++ src/App.js
@@ -63,26 +63,3 @@ class App extends Component {
 }
 
 export default App; 
-
-
-
-
-    // return (
-    //   <div className='App tc'>
-    //     <Navigation onRouteChange={this.onRouteChange}/>
-    //     { this.state.route === 'App'
-    //     ? <Landing 
-    //         className='home pa3'
-    //         onRsvpButtonSubmit={this.onRsvpButtonSubmit}/>
-    //     : <>{ this.state.route === 'MumbaiInfo'
-    //     ? <MumbaiInfo onRouteChange={this.onRouteChange}/>
-    //     : <>{this.state.route === 'PuneInfo'
-    //     ? <PuneInfo onRouteChange={this.onRouteChange}/>
-    //     : <Rsvp onRouteChange={this.onRouteChange}/>
-    //     }
-    //     </> 
-    //     }
-    //     </>
-    //     }
-    //   </div>
-    //   )
