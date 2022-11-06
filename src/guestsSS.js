ee31cf78df4a04693c73185df149a097dcb0b293
sanmays <sanmay3290@gmail.com>
2022-11-06 13:26:41 +0100

    Update guestlist

--- src/guestsSS.js
+++ src/guestsSS.js
@@ -1,62 +1,167 @@
 export const guestsSS = [
-{
-   id: 1,
+ {
+   Id: 1,
    LastName: "Bagwe",
    FirstName: "Sunil"
  },
  {
-   id: 2,
+   Id: 2,
    LastName: "Kulkarni",
    FirstName: "Parag"
  },
  {
-   id: 3,
+   Id: 3,
    LastName: "Sawant",
    FirstName: "Yashwant"
  },
  {
-   id: 4,
+   Id: 4,
    LastName: "Sawant",
    FirstName: "Ashish"
  },
  {
-   id: 5,
-   LastName: "Matnani",
-   FirstName: "Karan"
- },
- {
-   id: 6,
-   LastName: "VS",
-   FirstName: "Rutha"
- },
- {
-   id: 7,
-   LastName: "Naralkar",
-   FirstName: "Viren"
- },
- {
-   id: 8,
-   LastName: "Dandekar",
-   FirstName: "Nikhil"
- },
- {
-   id: 9,
+   Id: 5,
    LastName: "Rajhans",
    FirstName: "Sarang"
  },
  {
-   id: 10,
+   Id: 6,
    LastName: "Rajhans",
    FirstName: "Sandeep"
  },
  {
-   id: 11,
-   LastName: "Golatkar",
-   FirstName: "Atmanand"
+   Id: 7,
+   LastName: "Matnani",
+   FirstName: "Karan"
  },
  {
-   id: 12,
+   Id: 8,
+   LastName: "Acharya Matnani",
+   FirstName: "Sharada"
+ },
+ {
+   Id: 9,
+   LastName: "VS",
+   FirstName: "Rutha"
+ },
+ {
+   Id: 10,
+   LastName: "Bapat",
+   FirstName: "Mihir"
+ },
+ {
+   Id: 11,
+   LastName: "Naralkar",
+   FirstName: "Viren"
+ },
+ {
+   Id: 12,
+   LastName: "Naralkar",
+   FirstName: "Manasi"
+ },
+ {
+   Id: 13,
+   LastName: "Dandekar",
+   FirstName: "Nikhil"
+ },
+ {
+   Id: 14,
+   LastName: "Srushti",
+   FirstName: "Bhave"
+ },
+ {
+   Id: 15,
+   LastName: "Deshpande",
+   FirstName: "Hrishikesh"
+ },
+ {
+   Id: 16,
+   LastName: "Bhaduri",
+   FirstName: "Pronita"
+ },
+ {
+   Id: 17,
+   LastName: "Raje",
+   FirstName: "Shirin"
+ },
+ {
+   Id: 18,
+   LastName: "Bhogashetti",
+   FirstName: "Yogesh"
+ },
+ {
+   Id: 19,
+   LastName: "Kulkarni",
+   FirstName: "Amogh"
+ },
+ {
+   Id: 20,
+   LastName: "Gnanasambandham",
+   FirstName: "Chandramouli"
+ },
+ {
+   Id: 21,
+   LastName: "Patil",
+   FirstName: "Sheetal"
+ },
+ {
+   Id: 22,
+   LastName: "Dattatreya",
+   FirstName: "Karthik"
+ },
+ {
+   Id: 23,
+   LastName: "Parakh",
+   FirstName: "Viraj"
+ },
+ {
+   Id: 24,
+   LastName: "Matnani",
+   FirstName: "Priya"
+ },
+ {
+   Id: 25,
+   LastName: "Shiralkar",
+   FirstName: "Saurabh"
+ },
+ {
+   Id: 26,
+   LastName: "Torvekar",
+   FirstName: "Gaurang"
+ },
+ {
+   Id: 27,
+   LastName: "Kaluskar",
+   FirstName: "Sayalee"
+ },
+ {
+   Id: 28,
+   LastName: "Patwardhan",
+   FirstName: "Mandar"
+ },
+ {
+   Id: 29,
+   LastName: "Patwardhan",
+   FirstName: "Shraddha"
+ },
+ {
+   Id: 30,
+   LastName: "Huber",
+   FirstName: "Max"
+ },
+ {
+   Id: 31,
+   LastName: "Rupprecht",
+   FirstName: "Sarah"
+ },
+ {
+   Id: 32,
    LastName: "Golatkar",
    FirstName: "Sakshi"
- }
+ },
+ {
+   Id: 33,
+   LastName: "Golatkar",
+   FirstName: "Atmanand"
+ } 
 ]
